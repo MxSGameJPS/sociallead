6a769c0edaae8d9123b4b68f00e55352f6c92175
Saulo Pavanello <153878745+MxSGameJPS@users.noreply.github.com>
2026-08-03 00:27:17 -0300

    remove página de projetos e prévias

--- src/app/projetos/page.js
+++
@@ -1,9 +0,0 @@
-import { listSiteProjects } from "../../services/projects/projectStore.js";
-import ProjectsList from "../../components/ProjectsList/ProjectsList.jsx";
-
-export const dynamic = "force-dynamic";
-
-export default async function ProjectsPage() {
-  const projects = await listSiteProjects();
-  return <ProjectsList projects={projects} />;
-}
