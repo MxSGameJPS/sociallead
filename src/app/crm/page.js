9adc936b25f5e0c8a2b17b4237866749a8e73a34
Saulo Pavanello <153878745+MxSGameJPS@users.noreply.github.com>
2026-08-03 00:33:49 -0300

    feat: unify CRM and consulting data in one module

--- src/app/crm/page.js
+++ src/app/crm/page.js
@@ -1,15 +1,22 @@
 import { listLeads } from "../../repositories/leadRepository.js";
 import { getLeadWorkspace } from "../../services/workspaces/leadWorkspaceStore.js";
 import { resolveCommercialTrack, trackIncludes } from "../../services/leads/commercialTrack.js";
-import CRMBoard from "../../components/CRMBoard/CRMBoard.jsx";
+import CommercialHub from "../../components/CommercialHub/CommercialHub.jsx";
 
 export const dynamic = "force-dynamic";
 
 export default async function CRMPage() {
   const leads = await listLeads();
   const workspaces = await Promise.all(leads.map(lead => getLeadWorkspace(lead.id)));
-  const projectLeads = leads
-    .map((lead, index) => ({ ...lead, commercialTrack: resolveCommercialTrack(lead, workspaces[index]) }))
-    .filter(lead => trackIncludes(lead.commercialTrack, "projects"));
-  return <CRMBoard initialLeads={projectLeads} />;
+
+  const enriched = leads.map((lead, index) => ({
+    ...lead,
+    commercialTrack: resolveCommercialTrack(lead, workspaces[index]),
+    consulting: workspaces[index].consulting,
+  }));
+
+  const crmLeads = enriched.filter(lead => trackIncludes(lead.commercialTrack, "projects"));
+  const consultingLeads = enriched.filter(lead => trackIncludes(lead.commercialTrack, "consulting"));
+
+  return <CommercialHub crmLeads={crmLeads} consultingLeads={consultingLeads} />;
 }
