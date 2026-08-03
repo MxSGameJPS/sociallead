26e934a366a3946ca4d2879ff0ba55df73401f88
Saulo Pavanello <153878745+MxSGameJPS@users.noreply.github.com>
2026-08-03 00:34:34 -0300

    refactor: redirect consulting list to unified CRM

--- src/app/consultoria/page.js
+++ src/app/consultoria/page.js
@@ -1,17 +1,5 @@
-import { listLeads } from "../../repositories/leadRepository.js";
-import { getLeadWorkspace } from "../../services/workspaces/leadWorkspaceStore.js";
-import { resolveCommercialTrack, trackIncludes } from "../../services/leads/commercialTrack.js";
-import ConsultingBoard from "../../components/ConsultingBoard/ConsultingBoard.jsx";
+import { redirect } from "next/navigation";
 
-export const dynamic = "force-dynamic";
-
-export default async function ConsultingPage() {
-  const leads = await listLeads();
-  const workspaces = await Promise.all(leads.map(lead => getLeadWorkspace(lead.id)));
-  const consultingLeads = leads.map((lead, index) => ({
-    ...lead,
-    commercialTrack: resolveCommercialTrack(lead, workspaces[index]),
-    consulting: workspaces[index].consulting,
-  })).filter(lead => trackIncludes(lead.commercialTrack, "consulting"));
-  return <ConsultingBoard initialLeads={consultingLeads} />;
+export default function ConsultingPage() {
+  redirect("/crm");
 }
