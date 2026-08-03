7e96442c1b770641ab2b5e89757e0c38615265bd
Saulo Pavanello <153878745+MxSGameJPS@users.noreply.github.com>
2026-08-03 00:39:27 -0300

    refactor: make CRM the single lead board

--- src/app/crm/page.js
+++ src/app/crm/page.js
@@ -1,22 +1,9 @@
 import { listLeads } from "../../repositories/leadRepository.js";
-import { getLeadWorkspace } from "../../services/workspaces/leadWorkspaceStore.js";
-import { resolveCommercialTrack, trackIncludes } from "../../services/leads/commercialTrack.js";
-import CommercialHub from "../../components/CommercialHub/CommercialHub.jsx";
+import CRMBoard from "../../components/CRMBoard/CRMBoard.jsx";
 
 export const dynamic = "force-dynamic";
 
 export default async function CRMPage() {
   const leads = await listLeads();
-  const workspaces = await Promise.all(leads.map(lead => getLeadWorkspace(lead.id)));
-
-  const enriched = leads.map((lead, index) => ({
-    ...lead,
-    commercialTrack: resolveCommercialTrack(lead, workspaces[index]),
-    consulting: workspaces[index].consulting,
-  }));
-
-  const crmLeads = enriched.filter(lead => trackIncludes(lead.commercialTrack, "projects"));
-  const consultingLeads = enriched.filter(lead => trackIncludes(lead.commercialTrack, "consulting"));
-
-  return <CommercialHub crmLeads={crmLeads} consultingLeads={consultingLeads} />;
+  return <CRMBoard initialLeads={leads} />;
 }
